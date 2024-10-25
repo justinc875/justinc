75a4aa04951bfea0ea7595b335584d369f7148c4
BT_1N3_11 <sample@bths.edu>
2024-10-25 10:00:56 -0400

    initial commit added some logic

--- src/justinc.java
+++ src/justinc.java
@@ -1,5 +1,11 @@
 public class justinc {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        boolean junior = true;
+        if (junior = true) {
+            System.out.println("I am a junior in this school");
+        } else {
+            System.out.println("I am not a junior");
+        }
     }
 }
