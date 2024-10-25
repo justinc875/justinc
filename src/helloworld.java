e15655aaa8d5ecc6d089a9b22b1da3accb6532d0
BT_1N3_11 <sample@bths.edu>
2024-10-25 10:38:12 -0400

    removed task 1

--- src/helloworld.java
+++
@@ -1,8 +0,0 @@
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("i am taking csa right now");
-        System.out.println("this is 4th period");
-        System.out.println(9+10);
-    }
-}
