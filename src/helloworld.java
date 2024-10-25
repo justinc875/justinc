4a45b05c60c2f4c75a2623fa85251c828f839f44
BT_1N3_11 <sample@bths.edu>
2024-10-25 10:07:17 -0400

    added more print statements

---
+++ src/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("i am taking csa right now");
+        System.out.println("this is 4th period");
+    }
+}
